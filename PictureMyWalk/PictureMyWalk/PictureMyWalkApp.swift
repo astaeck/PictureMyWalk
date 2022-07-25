bd94bb4e50087a314eafcf2deabfaf108b4b0a0c
Angelina Staeck <angelina.staeck@gmail.com>
2022-07-25 12:46:16 +0200

    Initial commit

---
+++ PictureMyWalk/PictureMyWalk/PictureMyWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureMyWalkApp.swift
+//  PictureMyWalk
+//
+//  Created by Angelina Staeck on 25.07.22.
+//
+
+import SwiftUI
+
+@main
+struct PictureMyWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
